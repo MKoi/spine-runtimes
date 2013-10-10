883c79f4e3e829f4333449f743e381d8cbf324f6
MKoi <mikko.k.koivisto@gmail.com>
2013-10-10 22:14:33 +0300

    compiling version of spine-monogame-wp8 example

---
+++ spine-monogame-wp8/spine-monogame-wp8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using spine_monogame_wp8.Resources;
+
+namespace spine_monogame_wp8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
